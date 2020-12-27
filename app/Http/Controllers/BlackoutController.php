b1d0ad5053a27dd4e2068c0b1098b4369ddb194b
Marco Hernandez <marcoah@gmail.com>
2020-12-26 20:18:03 -0400

    Agregado blackout controller y recursos

---
+++ app/Http/Controllers/BlackoutController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Blackout;
+use Illuminate\Http\Request;
+
+class BlackoutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Blackout  $blackout
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Blackout $blackout)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Blackout  $blackout
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Blackout $blackout)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Blackout  $blackout
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Blackout $blackout)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Blackout  $blackout
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Blackout $blackout)
+    {
+        //
+    }
+}
