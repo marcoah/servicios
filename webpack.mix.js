080093ce7a51d659aae1d5a848456d8c940b1d5f
Marco Hernandez <marcoah@gmail.com>
2020-12-27 22:35:54 -0400

    Agregado Datatables y SB-ADMIN

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 const mix = require('laravel-mix');
 
+mix.setResourceRoot('../');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -13,3 +14,17 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+mix.copy('resources/js/sb-admin/sb-admin-2.min.js', 'public/js/sb-admin-2.min.js');
+mix.copy('resources/js/sb-admin/jquery.easing.min.js', 'public/js/jquery.easing.min.js');
+mix.copy('resources/js/sb-admin/demo/chart-area-demo.js', 'public/js/demo/chart-area-demo.js');
+mix.copy('resources/js/sb-admin/demo/chart-bar-demo.js', 'public/js/demo/chart-bar-demo.js');
+mix.copy('resources/js/sb-admin/demo/chart-pie-demo.js', 'public/js/demo/chart-pie-demo.js');
+
+mix.copy('node_modules/jquery/dist/jquery.min.js', 'public/js/jquery.min.js');
+mix.copy('node_modules/chart.js/dist/Chart.min.js', 'public/js/Chart.min.js');
+
+//Archivos Datatables
+mix.copy('node_modules/datatables.net/js/jquery.dataTables.min.js', 'public/js/jquery.dataTables.min.js');
+mix.copy('node_modules/datatables.net-dt/css/jquery.dataTables.min.css', 'public/css/jquery.dataTables.min.css');
+mix.copy('node_modules/chart.js/dist/Chart.min.js', 'public/js/dataTables.bootstrap4.min.js');
